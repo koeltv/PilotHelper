ad5ad56d8e513789c8d7cff90930f78b3076d36c
koeltv <Valentin.koeltgen@utt.fr>
2024-05-19 15:27:55 +0200

    Completed PDF filling Changed variable font to monospace font Added missing properties filling More precise filling Better tests

--- flight-plan/src/main/kotlin/com/pilothelper/FlightPlanPdfConverter.kt
+++ flight-plan/src/main/kotlin/com/pilothelper/FlightPlanPdfConverter.kt
@@ -10,6 +10,7 @@ import com.pilothelper.model.FlightPlan
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.util.*
 
 object FlightPlanPdfConverter {
     private const val PDF_FORM_NAME = "form/cerfa_14806-01.pdf"
@@ -17,14 +18,14 @@ object FlightPlanPdfConverter {
     fun fillFrom(flightPlan: FlightPlan): File? {
         return try {
             PdfReader(PDF_FORM_NAME).use { reader ->
-//                val outputFile = File.createTempFile("flight-plan-${UUID.randomUUID()}", ".pdf")
-                val outputFile = File("output.pdf")
+                val outputFile = File.createTempFile("flight-plan-${UUID.randomUUID()}", ".pdf")
                 println("PDF file: ${outputFile.absolutePath}")
 
                 PdfStamper(reader, FileOutputStream(outputFile)).use { stamper ->
                     val content = stamper.getOverContent(1)
 
-                    val baseFont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED)
+                    // We use courier because it is a Monospace font
+                    val baseFont = BaseFont.createFont(BaseFont.COURIER, BaseFont.CP1252, BaseFont.NOT_EMBEDDED)
                     content.beginText()
                     content.setFontAndSize(baseFont, 18f)
 
@@ -46,65 +47,143 @@ object FlightPlanPdfConverter {
 }
 
 /**
- * Fill the PDF form based on the [FlightPlan]
+ * Fill the PDF form based on the [FlightPlan].
+ * Text is placed based on coordinates. (0, 0) is the bottom-left of the page.
  *
  * @param flightPlan the data to fill the form with
  */
 private fun PdfContentByte.fillWithData(flightPlan: FlightPlan) {
-    characterSpacing = 7.0f
-
-    // 0, 0 is bottom left
-
-    // Expeditor
-    showTextAligned(ALIGN_LEFT, "Batman".uppercase(), 183f, 677f, 0f)
     // Case 7
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftData.aircraftId.uppercase(), 250f, 598f, 0f)
+    printSpacedText(flightPlan.aircraftData.aircraftId.uppercase(), 249f, 599f)
     // Case 8
-    showTextAligned(ALIGN_LEFT, flightPlan.flightRules.uppercase(), 451f, 598f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.flightType.uppercase(), 508f, 598f, 0f)
+    printSpacedText(flightPlan.flightRules.uppercase(), 452f, 599f)
+    printSpacedText(flightPlan.flightType.uppercase(), 509f, 599f)
     // Case 9
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftCount.toString().padStart(2, '0'), 65f, 558f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftData.aircraftType, 135f, 558f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftData.turbulenceType.uppercase(), 302f, 558f, 0f)
+    printSpacedText(flightPlan.aircraftCount.toString().padStart(2, '0'), 66f, 559f)
+    printSpacedText(flightPlan.aircraftData.aircraftType, 136f, 559f)
+    printSpacedText(flightPlan.aircraftData.turbulenceType.uppercase(), 303f, 559f)
     // Case 10
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftData.equipment, 405f, 545f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.aircraftData.transponder, 405f, 515f, 0f)
+    printText(flightPlan.aircraftData.equipment, 405f, 546f)
+    printText(flightPlan.aircraftData.transponder, 405f, 516f)
     // Case 13
-    showTextAligned(ALIGN_LEFT, flightPlan.startingAirport, 64f, 518f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.startingTime, 243f, 518f, 0f)
+    printSpacedText(flightPlan.startingAirport, 64f, 518f)
+    printSpacedText(flightPlan.startingTime, 243f, 518f)
     // Case 15
-    showTextAligned(ALIGN_LEFT, flightPlan.cruisingSpeed, 64f, 480f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.cruisingAltitude, 179f, 480f, 0f)
-    // TODO Add path/route
+    printSpacedText(flightPlan.cruisingSpeed, 63f, 481f)
+    printSpacedText(flightPlan.cruisingAltitude, 180f, 481f)
+    printParagraph(
+        flightPlan.path, listOf(
+            Line(294f, 480f, 26),
+            Line(30f, 461f, 55),
+            Line(30f, 443f, 55),
+            Line(30f, 425f, 51),
+        )
+    )
     // Case 16
-    showTextAligned(ALIGN_LEFT, flightPlan.destinationAirport, 64f, 380f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.estimatedTime, 187f, 380f, 0f)
+    printSpacedText(flightPlan.destinationAirport, 65f, 380f)
+    printSpacedText(flightPlan.estimatedTime, 186f, 380f)
     if (flightPlan.alternativeAirport.isNotEmpty()) {
-        showTextAligned(ALIGN_LEFT, flightPlan.alternativeAirport[0], 325f, 380f, 0f)
+        printSpacedText(flightPlan.alternativeAirport[0], 328f, 382f)
     }
-    if (flightPlan.alternativeAirport.size > 1) { // TODO
-        showTextAligned(ALIGN_LEFT, flightPlan.alternativeAirport[1], 420f, 380f, 0f)
+    if (flightPlan.alternativeAirport.size > 1) {
+        printSpacedText(flightPlan.alternativeAirport[1], 436f, 382f)
     }
+    // Case 18
+    printParagraph(
+        flightPlan.otherInformations.joinToString(" "), listOf(
+            Line(176f, 355f, 39),
+            Line(30f, 338f, 55),
+            Line(30f, 319f, 55),
+            Line(30f, 300f, 51),
+        )
+    )
     // Case 19
-    showTextAligned(ALIGN_LEFT, flightPlan.autonomy, 92f, 240f, 0f)
-    showTextAligned(ALIGN_LEFT, flightPlan.occupantCount.toString().padStart(3, '0'), 241f, 240f, 0f)
-    // TODO Other infos
+    printSpacedText(flightPlan.autonomy, 92f, 240f)
+    printSpacedText(flightPlan.occupantCount.toString().padStart(3, '0'), 241f, 241f)
 
-    if (!flightPlan.equipment.uhfPost)
-        drawMark(427f, 235f)
-    if (!flightPlan.equipment.vhfPost)
-        drawMark(471f, 235f)
-    if (!flightPlan.equipment.rdbaBeacon)
-        drawMark(527f, 235f)
+    fillEquipment(flightPlan.equipment)
 
-    if (!flightPlan.equipment.safetyJacketWithLight)
-        drawMark(387f, 183f)
-    if (!flightPlan.equipment.safetyJacketWithFluorescein)
-        drawMark(434f, 183f)
-    if (!flightPlan.equipment.safetyJacketWithUHF)
-        drawMark(484f, 183f)
-    if (!flightPlan.equipment.safetyJacketWithVHF)
-        drawMark(524f, 183f)
+    printText(flightPlan.aircraftData.colorAndMarkings, 96f, 109f)
+
+    if (flightPlan.remarks.isBlank()) drawMark(63f, 81f)
+    else printText(flightPlan.remarks, 96f, 82f)
+
+    printText(flightPlan.pilot, 96f, 55f)
+    printText(flightPlan.pilot, 35f, 25f)
+}
+
+/**
+ * Print all information related to the [FlightPlan.Equipment]
+ */
+private fun PdfContentByte.fillEquipment(equipment: FlightPlan.Equipment) {
+    if (!equipment.uhfPost) drawMark(427f, 235f)
+    if (!equipment.vhfPost) drawMark(471f, 235f)
+    if (!equipment.rdbaBeacon) drawMark(527f, 235f)
+
+    if (!equipment.survivalEquipmentPolar) drawMark(113f, 185f)
+    if (!equipment.survivalEquipmentDesert) drawMark(160f, 185f)
+    if (!equipment.survivalEquipmentMaritime) drawMark(207f, 185f)
+    if (!equipment.survivalEquipmentJungle) drawMark(252f, 185f)
+
+    if (!equipment.safetyJacketWithLight) drawMark(387f, 184f)
+    if (!equipment.safetyJacketWithFluorescein) drawMark(434f, 184f)
+    if (!equipment.safetyJacketWithUHF) drawMark(484f, 184f)
+    if (!equipment.safetyJacketWithVHF) drawMark(524f, 184f)
+
+    if (equipment.raftCount == 0) {
+        drawMark(65f, 140f)
+    } else {
+        printSpacedText(equipment.raftCount.toString().padStart(2, '0'), 99f, 142f)
+        printSpacedText(equipment.raftCapacity.toString().padStart(3, '0'), 165f, 142f)
+    }
+
+    if (equipment.raftCoverageColor.isBlank()) drawMark(245f, 140f)
+    else printText(equipment.raftCoverageColor, 302f, 142f)
+}
+
+/**
+ * Prints a [String] as a paragraph spread between the lines in the given order.
+ * Text is cut by spaces, that is to say a word won't be cut into parts.
+ */
+private fun PdfContentByte.printParagraph(paragraph: String, lines: List<Line>) {
+    val words = paragraph.split(" ")
+    val wordsPerLine = List(lines.size) { mutableListOf<String>() }
+    var currentLineIndex = 0
+    for (word in words) {
+        val currentLineLength =
+            wordsPerLine[currentLineIndex].sumOf { it.length } + wordsPerLine[currentLineIndex].size - 1
+        if (word.length + 1 + currentLineLength > lines[currentLineIndex].maxLength) {
+            currentLineIndex++
+            if (currentLineIndex > wordsPerLine.lastIndex) break
+        }
+        wordsPerLine[currentLineIndex] += word
+    }
+
+    for ((index, line) in lines.withIndex()) {
+        printText(wordsPerLine[index].joinToString(" "), line.x, line.y)
+    }
+}
+
+/**
+ * Represents a line on the form
+ */
+private data class Line(val x: Float, val y: Float, val maxLength: Int)
+
+/**
+ * Print text at given coordinates
+ */
+private fun PdfContentByte.printText(text: String, x: Float, y: Float) {
+    showTextAligned(ALIGN_LEFT, text, x, y, 0f)
+}
+
+/**
+ * Print text at given coordinates with spacing in between to fit in the form
+ */
+private fun PdfContentByte.printSpacedText(text: String, x: Float, y: Float) {
+    val originalSpacing = characterSpacing
+    characterSpacing = 7.0f
+    printText(text, x, y)
+    characterSpacing = originalSpacing
 }
 
 /**
@@ -116,6 +195,6 @@ private fun PdfContentByte.fillWithData(flightPlan: FlightPlan) {
 private fun PdfContentByte.drawMark(x: Float, y: Float) {
     setLineWidth(1.5f)
     moveTo(x, y)
-    lineTo(x+17, y+17)
+    lineTo(x + 17, y + 17)
     stroke()
 }
--- flight-plan/src/test/kotlin/com/pilothelper/FlightPlanPdfConverterTest.kt
+++ flight-plan/src/test/kotlin/com/pilothelper/FlightPlanPdfConverterTest.kt
@@ -3,50 +3,107 @@ package com.pilothelper
 import com.pilothelper.model.Aircraft
 import com.pilothelper.model.FlightPlan
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class FlightPlanPdfConverterTest {
     @Test
-    fun fillFromShouldWorkForValidData() {
+    fun fillFromShouldWorkForRealisticData() {
         val outputFile = FlightPlanPdfConverter.fillFrom(
             FlightPlan(
-                flightRules = 'C',
-                flightType = 'C',
+                flightRules = 'V',
+                flightType = 'G',
                 aircraftData = Aircraft(
-                    aircraftId = "ZZZZ",
-                    aircraftType = "D",
-                    turbulenceType = 'C',
-                    equipment = "F",
-                    transponder = "C",
-                    colorAndMarkings = "YELLOW"
+                    aircraftId = "FGKAO",
+                    aircraftType = "PA28",
+                    turbulenceType = 'L',
+                    equipment = "S",
+                    transponder = "S",
+                    colorAndMarkings = "BLANC"
                 ),
-                aircraftCount = 2,
-                startingAirport = "ZZZZ",
-                startingTime = "0123",
-                cruisingSpeed = "Z123",
-                cruisingAltitude = "D1245",
-                path = "This is a path",
-                destinationAirport = "ZZZZ",
-                estimatedTime = "0254",
-                alternativeAirport = listOf("ZZZZ"),
-                otherInformations = listOf(""),
-                autonomy = "0425",
-                occupantCount = 1,
-                pilot = "ME",
+                aircraftCount = 1,
+                startingAirport = "LFEB",
+                startingTime = "0900",
+                cruisingSpeed = "N0110",
+                cruisingAltitude = "F055",
+                path = "EEE FGD LEP MRP GDH GDF MZO DGC CKE QAJ OSJ KLZ SGC JEL",
+                destinationAirport = "LFRS",
+                estimatedTime = "0045",
+                alternativeAirport = listOf("LFRZ", "LFRI"),
+                otherInformations = listOf("DOF/220514 DOF/220514 DOF/220514 DOF/220514"),
+                autonomy = "0400",
+                occupantCount = 4,
+                pilot = "DUPONT",
+                remarks = "+33 06 XX XX XX XX",
                 equipment = FlightPlan.Equipment(
                     uhfPost = false,
                     vhfPost = false,
-                    rdbaBeacon = false,
-                    safetyJacketWithLight = false,
+                    rdbaBeacon = true,
+                    survivalEquipmentDesert = true,
+                    survivalEquipmentJungle = true,
+                    survivalEquipmentMaritime = true,
+                    survivalEquipmentPolar = true,
+                    safetyJacketWithLight = true,
                     safetyJacketWithFluorescein = false,
                     safetyJacketWithUHF = false,
                     safetyJacketWithVHF = false,
-                    raftCount = 2,
+                    raftCount = 0,
                     raftCapacity = 2,
-                    raftCoverageColor = "BLUE"
+                    raftCoverageColor = ""
                 )
             )
         )
         assertNotNull(outputFile)
+        assertEquals("pdf", outputFile.extension)
+    }
+
+    @Test
+    fun fillFromShouldWorkForValidData() {
+        val outputFile = FlightPlanPdfConverter.fillFrom(
+            FlightPlan(
+                flightRules = 'V',
+                flightType = 'G',
+                aircraftData = Aircraft(
+                    aircraftId = "FGKAO",
+                    aircraftType = "PA28",
+                    turbulenceType = 'L',
+                    equipment = "S",
+                    transponder = "S",
+                    colorAndMarkings = "BLANC"
+                ),
+                aircraftCount = 1,
+                startingAirport = "LFEB",
+                startingTime = "0900",
+                cruisingSpeed = "N0110",
+                cruisingAltitude = "F055",
+                path = "EEE FGD LEP MRP GDH GDF MZO DGC CKE QAJ OSJ KLZ SGC JEL",
+                destinationAirport = "LFRS",
+                estimatedTime = "0045",
+                alternativeAirport = listOf("LFRZ", "LFRI"),
+                otherInformations = listOf("DOF/220514 DOF/220514 DOF/220514 DOF/220514"),
+                autonomy = "0400",
+                occupantCount = 4,
+                pilot = "DUPONT",
+                remarks = "",
+                equipment = FlightPlan.Equipment(
+                    uhfPost = false,
+                    vhfPost = false,
+                    rdbaBeacon = false,
+                    survivalEquipmentDesert = false,
+                    survivalEquipmentJungle = false,
+                    survivalEquipmentMaritime = false,
+                    survivalEquipmentPolar = false,
+                    safetyJacketWithLight = false,
+                    safetyJacketWithFluorescein = false,
+                    safetyJacketWithUHF = false,
+                    safetyJacketWithVHF = false,
+                    raftCount = 1,
+                    raftCapacity = 2,
+                    raftCoverageColor = "BLEU"
+                )
+            )
+        )
+        assertNotNull(outputFile)
+        assertEquals("pdf", outputFile.extension)
     }
 }
