0adee31b88a00a80cab54faf9b169666d9cc53cd
koeltv <Valentin.koeltgen@utt.fr>
2024-06-02 22:00:22 +0200

    Added XML content type & updated OpenAPI

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktorVersion")
 
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
