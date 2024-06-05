8d39d18aaace23fedc5c7cc70589dad74d13a1ae
koeltv <Valentin.koeltgen@utt.fr>
2024-06-05 22:31:06 +0200

    Added Material support Changed home page to logo

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular
 import {provideRouter} from '@angular/router';
 import routeConfig from "./app.routes";
 import {provideServiceWorker} from '@angular/service-worker';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,6 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routeConfig), provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }), provideAnimationsAsync()
   ]
 };
