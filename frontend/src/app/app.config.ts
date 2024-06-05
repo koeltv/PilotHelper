b5103245246671e48b4afb62a8e23664c3175ca4
koeltv <Valentin.koeltgen@utt.fr>
2024-06-05 21:27:51 +0200

    Added PWA support Changed title and lang in index.html

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 import routeConfig from "./app.routes";
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routeConfig)
+    provideRouter(routeConfig), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
