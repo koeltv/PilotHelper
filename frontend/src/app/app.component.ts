d7d3d5158e95673b07c9881a6202bbc14bef3529
koeltv <Valentin.koeltgen@utt.fr>
2024-06-13 22:09:03 +0200

    Redo of the home screen & navigation system Added navbar for the whole application Removed unnecessary package-lock.json Better home screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { airCraft} from "../shared/models/airCraft";
-import { PersonalAircraftFormComponent} from "./create-aircraft-form/create-aicraft-form.component";
-import {NgForOf, CommonModule} from "@angular/common";
-import {showAircraftForm} from "./show-aircaft-form/show-aircraft-form.component";
-import { RouterModule} from "@angular/router";
-import routeConfig from "./app.routes";
+import {Component} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatAnchor, MatIconAnchor, MatIconButton} from "@angular/material/button";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, PersonalAircraftFormComponent, CommonModule, showAircraftForm, RouterModule],
+  imports: [MatToolbar, MatIcon, MatIconButton, MatAnchor, MatIconAnchor, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private router: Router) {
+  }
+
+  navigate(path: string) {
+    this.router.navigate([path]);
+  }
 }
