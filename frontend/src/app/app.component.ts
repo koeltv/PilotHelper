527a2dd0b2477c4171b35719cd0159de6251dc21
koeltv <Valentin.koeltgen@utt.fr>
2024-06-15 00:17:33 +0200

    Fixed angular material styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,12 +3,13 @@ import {Router, RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatAnchor, MatIconAnchor, MatIconButton} from "@angular/material/button";
+import {MatSidenavContainer} from "@angular/material/sidenav";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbar, MatIcon, MatIconButton, MatAnchor, MatIconAnchor, RouterOutlet],
+  imports: [MatToolbar, MatIcon, MatIconButton, MatAnchor, MatIconAnchor, RouterOutlet, MatSidenavContainer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
