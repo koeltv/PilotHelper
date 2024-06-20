5efd7ebc20203d574fe2586582da2b95ce0b84ed
koeltv <Valentin.koeltgen@utt.fr>
2024-06-20 20:08:38 +0200

    Modified aircraft form to work well with the backend & Applied material styling

--- frontend/src/app/aircraft-page/aircraft-page.component.ts
+++ frontend/src/app/aircraft-page/aircraft-page.component.ts
@@ -2,43 +2,43 @@ import {Component} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule, NgForOf} from "@angular/common";
 import {PersonalAircraftFormComponent} from "../create-aircraft-form/create-aicraft-form.component";
-import {showAircraftForm} from "../show-aircaft-form/show-aircraft-form.component";
+import {ShowAircraftForm} from "../show-aircaft-form/show-aircraft-form.component";
 import {AirCraft} from "../../shared/models/AirCraft";
 import {DataService} from "../api/data.service";
 
 @Component({
   selector: 'aircraft-page',
   standalone: true,
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgForOf,
-        PersonalAircraftFormComponent,
-        showAircraftForm,
-        ReactiveFormsModule
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgForOf,
+    PersonalAircraftFormComponent,
+    ShowAircraftForm,
+    ReactiveFormsModule
+  ],
   templateUrl: './aircraft-page.component.html',
   styleUrl: './aircraft-page.component.css'
 })
 export class AircraftPageComponent {
-
   airCraftList: AirCraft[] = [];
   visibleAircraft: AirCraft[] = [];
 
   constructor(private dataService: DataService) {
+    this.fetchAircrafts();
   }
 
-  addAircraft(aircraft: AirCraft) {
-    console.log('test', aircraft);
-    this.dataService.createAircraft(aircraft).subscribe(result=> console.log(result));
-
-  }
-
-  updateVisibleAircraft(updatedList: AirCraft[]) {
-    this.dataService.readAllAircraft().subscribe(allAircrafts =>{
-      this.airCraftList = allAircrafts;
-      this.visibleAircraft = updatedList;
+  private fetchAircrafts() {
+    this.dataService.readAllAircraft().subscribe(aircrafts => {
+      this.airCraftList = aircrafts;
     });
   }
 
+  addAircraft(aircraft: AirCraft) {
+    this.dataService.createAircraft(aircraft).subscribe(() => this.fetchAircrafts());
+  }
+
+  updateVisibleAircraft(updatedList: AirCraft[]) {
+    this.visibleAircraft = updatedList;
+  }
 }
--- frontend/src/app/show-aircaft-form/show-aircraft-form.component.ts
+++ frontend/src/app/show-aircaft-form/show-aircraft-form.component.ts
@@ -1,31 +1,37 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from "@angular/core";
 import {AirCraft} from "../../shared/models/AirCraft";
 import {PersonalAircraftFormComponent} from "../create-aircraft-form/create-aicraft-form.component";
 import {CommonModule} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'show-aircraft-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, PersonalAircraftFormComponent],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, PersonalAircraftFormComponent, MatLabel, MatSelect, MatOption, MatFormField],
   templateUrl: './show-aircraft-form.component.html',
   styleUrls: ['./show-aircraft-form.component.css']
 })
 
-export class showAircraftForm{
+export class ShowAircraftForm implements OnChanges {
 
-  @Input()airCraftList!: AirCraft[];
-  @Output()visibleAirCraftList = new EventEmitter<AirCraft[]>();
-  listFilter : string = '0';
-  visibleAircraft : AirCraft[] = this.airCraftList;
+  @Input() airCraftList!: AirCraft[];
+  @Output() updatedAircraftList = new EventEmitter<AirCraft[]>();
+  selected: AirCraft | undefined;
+  visibleAircrafts: AirCraft[] = this.airCraftList;
 
-  filterChange (value : any){
-    if (value === '0'){
-      this.visibleAircraft = this.airCraftList;
+  filterChange(value: AirCraft | undefined) {
+    if (value) {
+      this.visibleAircrafts = [value];
     } else {
-      this.visibleAircraft = [this.airCraftList[value-1]];
+      this.visibleAircrafts = this.airCraftList;
     }
-    this.visibleAirCraftList.emit(this.visibleAircraft);
+    this.updatedAircraftList.emit(this.visibleAircrafts);
   }
 
+  ngOnChanges(_changes: SimpleChanges): void {
+    this.selected = undefined;
+    this.filterChange(this.selected);
+  }
 }
