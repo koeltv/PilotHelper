fbfe1bd5c1521294ea1258a79d8c4833d1ff2f91
GwenRiou <gwenaelle.riou@utt.fr>
2024-06-20 22:27:34 +0200

    Update display-meteo.component.ts, clean code

--- frontend/src/app/flying-plan-page/display-meteo/display-meteo.component.ts
+++ frontend/src/app/flying-plan-page/display-meteo/display-meteo.component.ts
@@ -21,125 +21,110 @@ export class DisplayMeteoComponent implements OnChanges {
 
   @Input() airportCode!: string;
 
-    nameDisplay:string ="";
-    conditionDisplay:string = "";
-    temperatureDisplay:string = "";
-    dewpointDisplay:string = "";
-    pressureDisplay:string = "";
-    windsDisplay:string = "";
-    visibilityDisplay:string = "";
-    cloudsDisplay:string = "";
+  nameDisplay: string = "";
+  conditionDisplay: string = "";
+  temperatureDisplay: string = "";
+  dewpointDisplay: string = "";
+  pressureDisplay: string = "";
+  windsDisplay: string = "";
+  visibilityDisplay: string = "";
+  cloudsDisplay: string = "";
 
 
-   constructor(private planningToolService: PlanningToolsService) {
-   }
-
-  ngOnChanges(changes: SimpleChanges) {
-     if(this.airportCode.length==4){
-       this.displayWeatherFunction(this.airportCode);
-     }else{
-       this.nameDisplay = "Entrez un aéroport pour y voir la météo !";
-     }
+  constructor(private planningToolService: PlanningToolsService) {
   }
 
-   displayWeatherFunction(airportCode:string){
-      this.planningToolService.getWeatherInfoFor(airportCode).subscribe(info =>{
-        this.nameDisplay = `<b>METAR pour :</b> ${info.name} `;
-        this.conditionDisplay = `<b>Conditions à:</b> ${UnixToTimespan(info.obsTime)} `;
-        this.temperatureDisplay = `<b>Temperature:</b>	${info.temp}°C (${celsiusToFahrenheit(info.temp)}°F) `;
-        this.dewpointDisplay = `<b>Dewpoint:</b>	${info.dewp}°C (${celsiusToFahrenheit(info.dewp)} °F) `;
-        this.pressureDisplay = `<b>Pressure (altimeter):</b>	${mbToInchHg(info.altim)}  inches Hg (${info.altim} mb)`;
-        this.windsDisplay = `<b>Vents:</b> ${degToCompass(info.wdir)} (${info.wdir} degrés) à ${ktToMph(info.wspd)} MPH (${info.wspd} knots; ${ktToMs(info?.wspd)} m/s)`;
-        this.visibilityDisplay = `<b>Visibilité:</b>	${visibilityDecode(info.visib)}`;
-        this.cloudsDisplay = `<b>Nuages:</b> ${this.cloudInfoDisplay(info.clouds)}`;
-      })
-   }
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.airportCode.length == 4) {
+      this.displayWeatherFunction(this.airportCode);
+    } else {
+      this.nameDisplay = "Entrez un aéroport pour y voir la météo !";
+    }
+  }
 
-  protected readonly degToCompass = degToCompass;
-  protected readonly ktToMph = ktToMph;
-  protected readonly ktToMs = ktToMs;
-  protected readonly celsiusToFahrenheit = celsiusToFahrenheit;
+  displayWeatherFunction(airportCode: string) {
+    this.planningToolService.getWeatherInfoFor(airportCode).subscribe(info => {
+      this.nameDisplay = `<b>METAR pour :</b> ${info.name} `;
+      this.conditionDisplay = `<b>Conditions à:</b> ${this.unixToTimespan(info.obsTime)} `;
+      this.temperatureDisplay = `<b>Temperature:</b>	${info.temp}°C (${this.celsiusToFahrenheit(info.temp)}°F) `;
+      this.dewpointDisplay = `<b>Dewpoint:</b>	${info.dewp}°C (${this.celsiusToFahrenheit(info.dewp)} °F) `;
+      this.pressureDisplay = `<b>Pressure (altimeter):</b>	${this.mbToInchHg(info.altim)}  inches Hg (${info.altim} mb)`;
+      this.windsDisplay = `<b>Vents:</b> ${this.degToCompass(info.wdir)} (${info.wdir} degrés) à ${this.ktToMph(info.wspd)} MPH (${info.wspd} knots; ${this.ktToMs(info.wspd)} m/s)`;
+      this.visibilityDisplay = `<b>Visibilité:</b>	${this.visibilityDecode(info.visib)}`;
+      this.cloudsDisplay = `<b>Nuages:</b> ${this.cloudInfoDisplay(info.clouds)}`;
+    })
+  }
 
   cloudInfoDisplay(clouds: Cloud[] | undefined) {
     let cloudsSentence = "";
-    if(clouds){
-      clouds.forEach(cloud =>{
-        cloudsSentence += ` ${coverAbbreviationToCover(cloud.cover)} `
-        if(cloud.base !=null) {
-          cloudsSentence += `clouds at ${cloud.base} feet,`
+    if (clouds) {
+      clouds.forEach(cloud => {
+        if (cloud.base != null) {
+          cloudsSentence += `${this.coverAbbreviationToCover(cloud.cover)} à ${cloud.base} pieds,`
+        }else{
+          cloudsSentence += `${this.coverAbbreviationToCover(cloud.cover)}`
         }
       })
     }
     return cloudsSentence;
   }
 
-  protected readonly mbToInchHg = mbToInchHg;
-}
-
-
-function visibilityDecode(visibility:any):string{
-  if (visibility== "6+" ) {
-    return "6+ sm (10+ km)"
-  } else {
-    let visbilityInKm = (Number(visibility) * 1.60934).toFixed(2);
-    return `${visibility} sm ( ${visbilityInKm} km)`;
+  visibilityDecode(visibility: any): string {
+    if (visibility == "6+") {
+      return "6+ sm (10+ km)"
+    } else {
+      let visbilityInKm = (Number(visibility) * 1.60934).toFixed(2);
+      return `${visibility} sm ( ${visbilityInKm} km)`;
+    }
   }
-}
 
-function degToCompass(num: number) {
+  degToCompass(num: number) {
 
     var val = Math.floor((num / 22.5) + 0.5);
     var arr = ["Nord", "Nord-Nord-Est ", "Nord-Est ", "Est-Nord-Est ", "Est", "Est-Sud-Est ", "Sud-Est", "Sud-Sud-Est ", "Sud", "Susuroît", "Suroît", "Ouest-Suroît ", "Ouest", "Ouest-Noroît ", "Noroît", "Nord-Noroît "];
     return arr[(val % 16)];
+  }
 
-}
-function ktToMph(num: number) {
+  ktToMph(num: number) {
+    return (1.15 * num).toFixed(2);
+  }
 
-    return (1.15*num).toFixed(2);
+  ktToMs(num: number) {
+    return (0.514444 * num).toFixed(2);
+  }
 
-}
-function ktToMs(num: number) {
+  celsiusToFahrenheit(num: number) {
+    return (2 * num + 30).toFixed(2);
+  }
 
-    return (0.514444*num).toFixed(2);
-
-}
-
-function celsiusToFahrenheit(num: number) {
-
-    return (2*num+30).toFixed(2);
-
-}
-
-
-function coverAbbreviationToCover(coverShorthand:string){
-  switch (coverShorthand){
-    case "FEW": {
-      return "few"
-    }
-    case "SCT": {
-      return "scattered"
-    }
-    case "BKN": {
-      return "broken"
-    }
-    case "OVC": {
-      return "overcast"
-    }
-    case "CAVOK": {
-      return "ceiling and visibility are OK"
-    }
-    default:{
-      return "There has been a error"
+  coverAbbreviationToCover(coverShorthand: string) {
+    switch (coverShorthand) {
+      case "FEW": {
+        return "peu de nuages"
+      }
+      case "SCT": {
+        return "nuages épars"
+      }
+      case "BKN": {
+        return "nuages fragmentés"
+      }
+      case "OVC": {
+        return "couverts"
+      }
+      case "CAVOK": {
+        return "Le plafond et la visibilité sont OK"
+      }
+      default: {
+        return "Il y a eu une erreur."
+      }
     }
   }
-}
 
-function mbToInchHg (mb:number){
+  mbToInchHg(mb: number) {
+    return (mb / 33.8639).toFixed(2);
+  }
 
-    return (mb/33.8639).toFixed(2);
-
-}
-
-function UnixToTimespan(unix:number){
-  return new Date(unix);
+  unixToTimespan(unix: number) {
+    return new Date(unix);
+  }
 }
