8bb1e196c469427f546a7763711c7743d0503cd0
koeltv <Valentin.koeltgen@utt.fr>
2024-07-02 16:48:58 +0200

    Inlined Dockerfile start command

--- frontend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-envsubst < /usr/share/nginx/html/assets/env.js > /usr/share/nginx/html/assets/temp.js
-mv /usr/share/nginx/html/assets/temp.js /usr/share/nginx/html/assets/env.js
-exec nginx -g 'daemon off;'
